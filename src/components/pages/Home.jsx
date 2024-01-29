7c7a7ecf9e86c47621f74a3d7048968f5abf5895
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-01-29 17:20:48 +0100

    feat: add router configuration for home, logement, a-propos, and 404 page
    Added router configuration using React Router for the following paths:
    - '/' (Home)
    - '/logement' (FicheLogement)
    - '/a-propos' (APropos)
    - '*' (Page404)

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+      eveniet, eos dignissimos soluta nesciunt cupiditate molestias in
+      asperiores repudiandae aliquam dolores, quae enim vel unde dolore atque
+      rerum accusamus distinctio?
+    </div>
+  );
+}

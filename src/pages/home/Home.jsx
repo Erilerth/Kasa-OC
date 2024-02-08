c435b11d0f83c7a22aa36b45bf5758a56a350c55
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-02-08 23:45:14 +0100

    feat(Home.jsx): add links to logement details page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import Banner from '../../components/banner/Banner';
 import Card from '../../components/Card/Card';
 import LogementContext from '../../context/LogementContext';
@@ -14,10 +15,12 @@ export default function Home() {
         <Banner />
         <section className='logementsCards'>
           {logement.map((logement) => (
-            <Card key={logement.id}>
-              <img src={logement.cover} alt={logement.title} />
-              <h2>{logement.title}</h2>
-            </Card>
+            <Link to={`/logement/${logement.id}`} key={logement.id}>
+              <Card>
+                <img src={logement.cover} alt={logement.title} />
+                <h2>{logement.title}</h2>
+              </Card>
+            </Link>
           ))}
         </section>
       </main>

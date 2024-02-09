285087f80f9af57ed4d1d3cb95b3742314803550
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-02-09 21:56:44 +0100

    feat(Collapsible, A-Propos): add PropTypes to Collapsible and populate A-Propos with data

--- src/components/Collapsible/Collapsible.jsx
+++ src/components/Collapsible/Collapsible.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import './_collapsible.scss';
 
-export default function Collapsible() {
+export default function Collapsible(props) {
   const [open, setOpen] = useState(false);
 
   function toggle() {
@@ -11,9 +12,14 @@ export default function Collapsible() {
   return (
     <div>
       <button onClick={toggle}>
-        toggle <i className='fa-solid fa-angle-up'></i>
+        {props.label} <i className='fa-solid fa-angle-up'></i>
       </button>
-      {open && <div className='collapse-content'>content</div>}
+      {open && <div className='collapse-content'>{props.children}</div>}
     </div>
   );
 }
+
+Collapsible.propTypes = {
+  label: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
--- src/pages/A-Propos/A-Propos.jsx
+++ src/pages/A-Propos/A-Propos.jsx
@@ -1,12 +1,18 @@
 import Banner from '../../components/Banner/Banner';
 import Collapsible from '../../components/Collapsible/Collapsible';
+import AboutArray from '../../data/aboutArray.json';
+import './_a-propos.scss';
 
 function APropos() {
   return (
     <main className='container'>
       <Banner />
-      <section id='collapsible'>
-        <Collapsible />
+      <section className='collapsible'>
+        {AboutArray.map((rule, id) => (
+          <Collapsible key={id} label={rule.Title}>
+            <p>{rule.Text}</p>
+          </Collapsible>
+        ))}
       </section>
     </main>
   );

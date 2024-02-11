67bb2c339a7154ad31c8275a18f943a4be37332a
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-02-11 19:08:23 +0100

    feat: Add dynamic logement data retrieval

--- src/pages/Fiche-Logement/Fiche-Logement.jsx
+++ src/pages/Fiche-Logement/Fiche-Logement.jsx
@@ -1,4 +1,11 @@
+import { useParams } from 'react-router-dom';
+import LogementData from '../../data/logements.json';
+
 function FicheLogement() {
+  const { id } = useParams();
+
+  const currentLogement = LogementData.find((logement) => logement.id === id);
+
   return (
     <>
       <p>Fiche-Logement</p>

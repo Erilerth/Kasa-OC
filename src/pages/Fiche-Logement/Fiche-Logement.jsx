6611002e2b3433208f755cb43ffb1a42d82a753a
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-02-11 20:34:37 +0100

    feat: add Fiche-Logement component and styles

--- src/pages/Fiche-Logement/Fiche-Logement.jsx
+++ src/pages/Fiche-Logement/Fiche-Logement.jsx
@@ -1,40 +1,66 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import LogementData from '../../data/logements.json';
+import './_fiche-logement.scss';
 
-function FicheLogement() {
+export default function FicheLogement() {
   const { id } = useParams();
   const currentLogement = LogementData.find((logement) => logement.id === id);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const currentPicture = currentLogement.pictures;
+  const currentPictures = currentLogement.pictures;
 
-  const nextImage = () => {
+  function nextImage() {
     setCurrentImageIndex(
-      (prevIndex) => (prevIndex + 1) % currentPicture.length
+      (prevIndex) => (prevIndex + 1) % currentPictures.length
     );
-  };
+  }
 
-  const prevImage = () => {
+  function prevImage() {
     setCurrentImageIndex(
       (prevIndex) =>
-        (prevIndex - 1 + currentPicture.length) % currentPicture.length
+        (prevIndex - 1 + currentPictures.length) % currentPictures.length
     );
-  };
+  }
 
   return (
-    <main>
-      {currentPicture.length > 0 && (
-        <div>
-          <button onClick={prevImage}>Previous</button>
+    <main id='fiche-logement' className='container'>
+      {currentPictures.length > 0 && (
+        <div id='carousel'>
+          <button
+            onClick={prevImage}
+            className='fa-solid fa-angle-left'></button>
           <img
-            src={currentPicture[currentImageIndex]}
+            src={currentPictures[currentImageIndex]}
             alt={`Slide ${currentImageIndex}`}
           />
-          <button onClick={nextImage}>Next</button>
+          <button
+            onClick={nextImage}
+            className='fa-solid fa-angle-right'></button>
         </div>
       )}
+
+      <section id='logement-infos'>
+        <h2 id='logement-title'>{currentLogement.title}</h2>
+        <h3 id='logement-location'>{currentLogement.location}</h3>
+        <div id='logement-tags'>
+          {currentLogement.tags.map((tag, index) => (
+            <p key={index} className='tag'>
+              {tag}
+            </p>
+          ))}
+        </div>
+      </section>
+
+      <section id='rating-host'>
+        <div id='host'>
+          <p>{currentLogement.host.name}</p>
+          <img
+            src={currentLogement.host.picture}
+            alt={`Image de ${currentLogement.host.name}`}
+          />
+        </div>
+      </section>
+      <section id='logement-description'></section>
     </main>
   );
 }
-
-export default FicheLogement;

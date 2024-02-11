d1b4fb2d40524588b730c38eaa03fb6142475ce3
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-02-11 19:24:03 +0100

    feat(FicheLogement.jsx): add image slideshow functionality

--- src/pages/Fiche-Logement/Fiche-Logement.jsx
+++ src/pages/Fiche-Logement/Fiche-Logement.jsx
@@ -1,15 +1,39 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import LogementData from '../../data/logements.json';
 
 function FicheLogement() {
   const { id } = useParams();
-
   const currentLogement = LogementData.find((logement) => logement.id === id);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const currentPicture = currentLogement.pictures;
+
+  const nextImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex + 1) % currentPicture.length
+    );
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + currentPicture.length) % currentPicture.length
+    );
+  };
 
   return (
-    <>
-      <p>Fiche-Logement</p>
-    </>
+    <main>
+      {currentPicture.length > 0 && (
+        <div>
+          <button onClick={prevImage}>Previous</button>
+          <img
+            src={currentPicture[currentImageIndex]}
+            alt={`Slide ${currentImageIndex}`}
+          />
+          <button onClick={nextImage}>Next</button>
+        </div>
+      )}
+    </main>
   );
 }
 

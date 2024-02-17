f49fe9218b8d17737abbbb77c2ec6b0937067d69
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-02-17 17:50:01 +0100

    feat: add responsive design for Fiche-Logements

--- src/pages/Fiche-Logement/Fiche-Logement.jsx
+++ src/pages/Fiche-Logement/Fiche-Logement.jsx
@@ -45,41 +45,45 @@ export default function FicheLogement() {
         </div>
       )}
 
-      <section id='logement-infos'>
-        <h2 id='logement-title'>{currentLogement.title}</h2>
-        <h3 id='logement-location'>{currentLogement.location}</h3>
-        <div id='logement-tags'>
-          {currentLogement.tags.map((tag, index) => (
-            <p key={index} className='tag'>
-              {tag}
-            </p>
-          ))}
-        </div>
-      </section>
-
-      <section id='rating-host'>
-        <div id='rating'>
-          {[...Array(5)].map((_, index) => {
-            return (
-              <span
-                key={index}
-                className={`fa-solid fa-star ${
-                  index < Number(currentLogement.rating) ? 'active' : 'inactive'
-                }`}></span>
-            );
-          })}
-        </div>
-        <div id='host'>
-          <div>
-            <p>{firstName}</p>
-            <p>{lastName}</p>
+      <div className='infos'>
+        <section id='logement-infos'>
+          <h2 id='logement-title'>{currentLogement.title}</h2>
+          <h3 id='logement-location'>{currentLogement.location}</h3>
+          <div id='logement-tags'>
+            {currentLogement.tags.map((tag, index) => (
+              <p key={index} className='tag'>
+                {tag}
+              </p>
+            ))}
           </div>
-          <img
-            src={currentLogement.host.picture}
-            alt={`Image de ${currentLogement.host.name}`}
-          />
-        </div>
-      </section>
+        </section>
+
+        <section id='rating-host'>
+          <div id='rating'>
+            {[...Array(5)].map((_, index) => {
+              return (
+                <span
+                  key={index}
+                  className={`fa-solid fa-star ${
+                    index < Number(currentLogement.rating)
+                      ? 'active'
+                      : 'inactive'
+                  }`}></span>
+              );
+            })}
+          </div>
+          <div id='host'>
+            <div>
+              <p>{firstName}</p>
+              <p>{lastName}</p>
+            </div>
+            <img
+              src={currentLogement.host.picture}
+              alt={`Image de ${currentLogement.host.name}`}
+            />
+          </div>
+        </section>
+      </div>
 
       <section id='logement-description' className='collapsible'>
         <Collapsible
